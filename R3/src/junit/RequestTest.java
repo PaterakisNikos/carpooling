6baab95dcd15c93cdfb9a3bd4279d04e5f6f07d0
VMormoris <48360903+VMormoris@users.noreply.github.com>
2019-04-20 18:25:54 +0300

    Add files via upload

---
+++ R3/src/junit/RequestTest.java
@@ -0,0 +1,34 @@
+package com.exercise.softeng;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Vasilis on 4/20/2019.
+ */
+public class RequestTest {
+
+    @Test
+    public void testRequestConstructorSuccess(){
+        Passenger input_passenger=new Passenger("ElpsyCongroo","pass");
+        LatLng input_position=new  LatLng(38.049186, 23.835265);
+        Request req=new Request(input_position, input_passenger.getUsername());
+        assertTrue("Upon successfull creation of request req.creation_successfull must return true", req.creation_successfull());
+        assertEquals("After successfull creation of request, req.posiotion should match input_position", input_position, req.position);
+        assertEquals("After successfull creation of request, req.passenger should match input_passenger.getUsername()", input_passenger.getUsername(), req.passenger);
+
+    }
+
+    @Test
+    public void testRequestConstructorFail(){
+        Passenger input_passenger=new Passenger("ElpsyCongroo","pass");
+        LatLng input_position=new  LatLng(38.049186, 23.835265);
+        Request req=new Request(input_position, "kitsos");
+        assertFalse("Username that is not used by a user should not create a request", req.creation_successfull());
+        req=new Request(null, input_passenger.getUsername());
+        assertFalse("null position should not create request", req.creation_successfull());
+    }
+}
